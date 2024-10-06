866a8e3d733da0d9e607a4a2fbf99cd30deffe91
faysal-islam-fd <fahadiislam01@gmail.com>
2024-10-05 23:56:16 -0700

    recipe api created

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 import express, { urlencoded } from 'express';
 import { connectDB } from './db/connectDB.js';
 import dotenv from 'dotenv/config';
+import cookieParser from 'cookie-parser';
+import recipeRoutes from './routes/recipe.routes.js';
 import cors from 'cors';
 
 import userRoutes from './routes/user.routes.js';
 const app = express();
 
 app.use(cors())
+app.use(cookieParser());
 app.use(urlencoded({extended: true}));
 app.use(express.json());
 app.use('/api/auth',userRoutes)
+app.use('/api/recipe',recipeRoutes)
 
 const PORT = process.env.PORT || 5000;
 
